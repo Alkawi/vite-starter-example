6203444d215585458a91ce83209f2c54c68a26bc
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-10-23 10:42:29 +0200

    Style navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,17 @@ import { createElement } from '../lib/dom.js'
 import './Navigation.css'
 
 export default function Navigation() {
-  const el = createElement('nav', { className: 'Navigation' })
+  const el = createElement(
+    'nav',
+    { className: 'Navigation' },
+    createElement('button', {
+      className: 'Navigation__button Navigation__button--active',
+      innerText: 'Characters',
+    }),
+    createElement('button', {
+      className: 'Navigation__button',
+      innerText: 'Locations',
+    })
+  )
   return el
 }
